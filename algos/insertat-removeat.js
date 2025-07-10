3340ac2725e96f68b176179034ee24a7acd55858
narcisolobo <narcisolobo@gmail.com>
2025-07-10 08:16:42 -0700

    insertAt removeAt solutions

--- algos/insertat-removeat.js
+++ algos/insertat-removeat.js
@@ -1,16 +1,24 @@
-/* 
-  insertAt
-  Given an array, index, and additional value, ​insert
-  the value into the array a​t the given index. Do this
-  without using built-in array methods. Return the
-  array. Do this in-place (no temp arrays).
-  
-  You can think of ​pushFront(arr, val)​ as equivalent to
-  insertAt(arr, 0, val)​.
-*/
-
+/**
+ * insertAt
+ * Given an array, index, and additional value, ​insert
+ * the value into the array a​t the given index. Do this
+ * without using built-in array methods. Return the
+ * array. Do this in-place (no temp arrays).
+ *
+ * You can think of ​pushFront(arr, val)​ as equivalent to
+ * insertAt(arr, 0, val)​.
+ *
+ * @param {Number[]} arr
+ * @param {Number} idx
+ * @param {Number} val
+ * @returns {Number}
+ */
 function insertAt(arr, idx, val) {
-  // your code here
+  for (let i = arr.length; i > idx; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[idx] = val;
+  return arr;
 }
 
 const idx = 2;
@@ -28,8 +36,33 @@ console.log(insertAt(myArr, idx, val)); // [1, 2, 6, 3, 4, 5]
   Think of popFront(arr)​ as equivalent to removeAt(arr, 0).​
 */
 
+/**
+ * removeAt
+ * Given an array and an index into the array, remove and
+ * return the array value ​at that index. Do this without
+ * using any built-in array methods except ​pop()​. Do this
+ * in-place (no temp arrays). Log the array after mutation.
+ *
+ * Think of popFront(arr)​ as equivalent to removeAt(arr, 0).​
+ *
+ * @param {Number[]} arr
+ * @param {Number} idx
+ * @returns {Number}
+ */
 function removeAt(arr, idx) {
-  // your code here
+  if (arr.length === 0) {
+    console.log('The array is empty.');
+    return;
+  }
+
+  const popped = arr[idx];
+  for (let i = idx; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+
+  arr.pop();
+  console.log(arr);
+  return popped;
 }
 
 console.log(removeAt([1, 2, 3, 4, 5], 2)); // [1, 2, 4, 5]
